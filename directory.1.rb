0bababc9256122866738dc1f6699004678581ea4
aryadath <ec2-user@ip-172-31-83-175.ec2.internal>
2023-03-09 16:27:48 +0000

    Added hobbies and height

--- directory.1.rb
+++ directory.1.rb
@@ -1,24 +1,30 @@
 def input_students
-    puts "Please enter the students name"
-    puts "To finish, hit return twice"
+  puts "Please enter the student's name:"
+  puts "To finish, hit return twice"
 
-    # create an empty array
-    students = []
+  # create an empty array
+  students = []
 
-    # get the user input (first name)
+  # get the user input (first name)
+  name = gets.chomp
+
+  # for when the code is not empty
+  while !name.empty? do
+    # add the student hash to the array
+    puts "Please enter the student's hobbies:"
+    hobbies = gets.chomp
+    puts "Please enter the student's height:"
+    height = gets.chomp.to_i
+    students << {name: name, cohort: :november, hobbies: hobbies, height: height}
+    puts "Now we have #{students.count} students"
+    # get another name from user
     name = gets.chomp
+  end
 
-    # for when the code is not empty
-    while !name.empty? do
-       # add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-       # get another name from user
-      name = gets.chomp
-    end
-    # return array of students
-    students
+  # return array of students
+  students
 end
+
 
 def print_header
     puts "The students of Villians Academy"
@@ -26,16 +32,12 @@ def print_header
 end
 
 # adjusted to print those less than 12
-def print_students_with_short_name(students)
-  index = 0
-  while index < students.length
-    student = students[index]
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
-    index += 1
+def print(students)
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) - Hobbies: #{student[:hobbies]}, Height: #{student[:height]} cm"
   end
 end
+
 
 
 def print_footer(names)
